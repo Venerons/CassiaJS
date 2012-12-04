11fd33a087f504c5d2e6b6f623d20bc87b516801
Daniele Veneroni <Venerons@MacBook-Pro-di-Daniele-Veneroni.local>
2012-12-04 15:37:44 +0100

    Initial Commit

---
+++ cassia.js
@@ -0,0 +1,38 @@
+var cesare = {
+    encrypt: function(messaggio) {
+        alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        beta = "DEFGHIJKLMNOPQRSTUVWXYZABCdefghijklmnopqrstuvwxyzabc3456789012";
+        ris = "";
+        for (i = 0; i < messaggio.length; i++) {
+            c = messaggio.charAt(i);
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                ord = alfa.indexOf(c);
+                ris += beta.charAt(ord);
+            } else {
+                ris += c;
+            }
+        }
+        return ris;
+    },
+    decrypt: function(messaggio) {
+        alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        beta = "DEFGHIJKLMNOPQRSTUVWXYZABCdefghijklmnopqrstuvwxyzabc3456789012";
+        ris = "";
+        for (i = 0; i < messaggio.length; i++) {
+            c = messaggio.charAt(i);
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                ord = beta.indexOf(c);
+                ris += alfa.charAt(ord);
+            } else {
+                ris += c;
+            }
+        }
+        return ris;
+    }
+};
+
+function Chipers() {
+    this.cesare = cesare;
+}
+
+var Cassia = cassia = new Chipers();
